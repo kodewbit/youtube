e9699f1109b8543b1729e385b82d9f60dcb66f76
Leonardo Yanes <leo95batista@gmail.com>
2021-03-09 12:39:43 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## About
+
+A small library that acts as a Laravel wrapper for YouTube data API v3.
+
+## License
+
+Meteor is open-sourced software licensed under the [MIT license](LICENSE.md).
---
+++ src/YouTubeServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Kodewbit\YouTube;
+
+use Illuminate\Support\ServiceProvider;
+
+class YouTubeServiceProvider extends ServiceProvider
+{
+    /**
+     * @inheritdoc
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom($this->getConfigurationFile(), 'youtube');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                $this->getConfigurationFile() => config_path('youtube.php')
+            ], 'youtube-config');
+        }
+    }
+
+    /**
+     * Get the package configuration file
+     *
+     * @return false|string
+     */
+    public function getConfigurationFile()
+    {
+        return realpath($raw = __DIR__ . '/../config/youtube.php') ?: $raw;
+    }
+}
